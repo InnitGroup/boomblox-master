804b371ba3fbec67092af0d30be62d2e8a47e85f
marsoc <mlgmusic445@gmail.com>
2025-08-22 13:57:53 -0500

    Added user version of gameserver.ashx for studio testing

--- Game/gameserver.php
+++ Game/gameserver.php
@@ -2,19 +2,20 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
 header("Content-Type: text/plain");
 global $user, $auth;
-Server::ipLock();
-
-if (!Server::isLocal()) {
-    exit;
-}
 
 $port = $_GET["serverPort"] ?? 10000;
 $place = $_GET["PlaceID"] ?? 1;
 
-if ($auth->isAuthed()) {
-    echo 1;
+if (!Server::isLocal()) {
+    $file = new File("/api/private/lua/usergameserver.lua", [
+        "Url" => url,
+        "Port" => 56430
+    ]);
+    echo $file->handle();
     exit;
 }
+
+Server::ipLock();
 
 if (Setting::disabled("Gameservers")) {
     exit;
---
+++ api/private/lua/usergameserver.lua
@@ -0,0 +1,107 @@
+-- declarations
+local port = {Port}
+local sleepTime = 10
+
+-- establish this peer as the Server
+local ns = game:GetService("NetworkServer")
+
+-- utility
+function waitForChild(parent, childName)
+	while true do
+		local child = parent:FindFirstChild(childName)
+		if child then
+			return child
+		end
+		parent.ChildAdded:Wait()
+	end
+end
+
+-- returns the player object that killed this humanoid
+-- returns nil if the killer is no longer in the game
+function getKillerOfHumanoidIfStillInGame(humanoid)
+
+	-- check for kill tag on humanoid - may be more than one - todo: deal with this
+	local tag = humanoid:FindFirstChild("creator")
+
+	-- find player with name on tag
+	if tag then
+		local killer = tag.Value
+		if killer.Parent then -- killer still in game
+			return killer
+		end
+	end
+
+	return nil
+end
+
+-- send kill and death stats when a player dies
+function onDied(victim, humanoid)
+	local killer = getKillerOfHumanoidIfStillInGame(humanoid)
+
+	local victorId = 0
+	if killer then
+		victorId = killer.userId
+		print("STAT: kill by " .. victorId .. " of " .. victim.userId)
+	end
+	print("STAT: death of " .. victim.userId .. " by " .. victorId)
+end
+
+-- listen for the death of a Player
+function createDeathMonitor(player)
+	-- we don't need to clean up old monitors or connections since the Character will be destroyed soon
+	if player.Character then
+		local humanoid = waitForChild(player.Character, "Humanoid")
+		humanoid.Died:connect(
+			function ()
+				onDied(player, humanoid)
+			end
+		)
+	end
+end
+
+-- listen to all Players' Characters
+game:service("Players").ChildAdded:connect(
+	function (player)
+		createDeathMonitor(player)
+		player.Changed:connect(
+			function (property)
+				if property=="Character" then
+					createDeathMonitor(player)
+				end
+			end
+		)
+	end
+)
+
+-- This code might move to C++
+function characterRessurection(player)
+	if player.Character then
+		local humanoid = player.Character.Humanoid
+		humanoid.Died:connect(function() wait(5) player:LoadCharacter() end)
+	end
+end
+
+game:service("Players").PlayerAdded:connect(function(player)	
+	print("Player " .. player.userId .. " added")
+
+	characterRessurection(player)
+	player.Changed:connect(function(name)
+		if name=="Character" then
+			characterRessurection(player)
+		end
+	end)
+
+	player.Chatted:connect(function(msg)  
+		if (msg == ";wm") then
+			player.Character.Humanoid.Health = 0
+		end
+	end)
+
+end)
+
+if port>0 then
+	-- Now start the connection
+	ns:Start(port, sleepTime) 
+end
+
+game:GetService("RunService"):Run()
