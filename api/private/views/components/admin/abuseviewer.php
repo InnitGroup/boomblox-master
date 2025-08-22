f6449caad88c2c9c1b7f29a96b584e849285b154
marsoc <mlgmusic445@gmail.com>
2025-08-21 19:18:58 -0500

    Asset report handling

--- api/private/views/components/admin/abuseviewer.php
+++ api/private/views/components/admin/abuseviewer.php
@@ -1,6 +1,7 @@
 <?php
 $reportedAbuse;
-switch ($abuse["type"]) {
+$type = $abuse["type"];
+switch ($type) {
     case "user":
         #$type = "/aaa/images/user.png";
         $reportedAbuse = "/User.aspx?ID=".$abuse["abuse"];
@@ -17,25 +18,37 @@ switch ($abuse["type"]) {
 
 <div id="MainPanel">
     <div id="Report" style="border:solid 1px black; background-color:white; padding:15px; width:fit-content; text-align:center;">
-        <?php if ($abuse["type"] !== "chat"): ?>
+        <?php if ($type !== "chat"): ?>
             <?php 
             $thumb;
             $render;
-            $abuse["type"] == "user" ? $thumb = new Avatar($abuse["abuse"]) : $thumb = new Asset($abuse["abuse"]);
-            $abuse["type"] == "user" ? $render = $thumb->GetThumbnail(500, 500, "PNG") : $render = $thumb->getThumbnail(250, 250, "PNG");
+            $type == "user" ? $thumb = new Avatar($abuse["abuse"]) : $thumb = new Asset($abuse["abuse"]);
+            $type == "user" ? $render = $thumb->GetThumbnail(500, 500, "PNG") : $render = $thumb->getThumbnail(250, 250, "PNG");
             ?>
             <img src="<?=$render?>" style="height:250px;">
         <?php endif; ?>
         <p>Abuse ID: <?=$abuse["id"]?></p>
-        <p>Type: <?=ucfirst($abuse["type"])?></p>
-        <p>Reported Abuse: <a href="<?=$reportedAbuse?>" target="_blank"><?=ucfirst($abuse["type"])?></a></p>
+        <p>Type: <?=ucfirst($type)?></p>
+        <p>Reported Abuse: <a href="<?=$reportedAbuse?>" target="_blank"><?=ucfirst($type)?></a></p>
         <p>Informant Comment: <?=htmlspecialchars($abuse["comment"])?></p>
         <p>Informant: <a href="/User.aspx?ID=<?=$abuse["reportedBy"]?>" target="_blank">User</a></p>
         <p>Handled: <img style="position:relative;top:3px;" src="<?=$abuse["handled"] == 1 ? "/aaa/images/accepted.png" : "/aaa/images/stop.png"?>"></p>
         <p>Reported At: <?=$abuse["date"]?></p>
 
         <?php if ($abuse["handled"] == 0): ?>
+        <?php if ($type == "user" || $type == "chat"): ?>
         <button type="button" onclick="window.location.href = '/Admi/Users/ModerateUser.aspx?UserID=<?=$abuse['abuse']?>&AbuseID=<?=$abuse['id']?>';" style="width:230px;">Handle</button>
+        <?php elseif ($type == "asset"): ?>
+        <?php 
+        global $db;
+        $stmt = "SELECT creatorId FROM items WHERE itemId=:itemId";
+        $result = $db->execute($stmt, [":itemId" => $abuse['abuse']]);
+        $fetched = $result->fetch(PDO::FETCH_ASSOC);
+        $creatorId = $fetched["creatorId"];
+        ?>
+        <button type="button" onclick="window.location.href = '/Admi/Users/ModerateUser.aspx?UserID=<?=$creatorId?>&AbuseID=<?=$abuse['id']?>';" style="width:230px;">Handle via User</button><br><br>
+        <button type="button" onclick="window.location.href = '/Admi/Moderation/BlockAsset.aspx?AssetID=<?=$abuse['abuse']?>&AbuseID=<?=$abuse['id']?>';" style="width:230px;">Handle via Asset</button>
+        <?php endif; ?>
         <?php endif; ?>
     </div>
 </div>
--- api/private/views/components/admin/blockasset.php
+++ api/private/views/components/admin/blockasset.php
@@ -2,7 +2,7 @@
     <h1>Block Asset</h1>
     <p>Do not intentionally try to break this panel as it was made within 5 minutes.</p>
     <label>Asset ID: </label>
-    <input type="number" name="ctl$cphRoblox$AssetID"><br>
+    <input type="number" name="ctl$cphRoblox$AssetID" value="<?=isset($_GET["AssetID"]) ? (int)$_GET["AssetID"] : ""?>"><br>
     <input type="submit" value="Block">
     <?php
     global $db, $user;
@@ -13,6 +13,14 @@
                 $stmt = "UPDATE items SET `status`='blocked',`lastUpdate`=:lastUpdate WHERE itemId=:itemId";
                 $db->execute($stmt, [":itemId" => $assetId, ":lastUpdate" => date('Y-m-d H:i:s')]);
                 echo "Successfully blocked!";
+                if (isset($_GET["AssetID"]) && isset($_GET["AbuseID"])) {
+                    $stmt = "SELECT * FROM reports WHERE `handled`=0 AND `abuse`=:assetId AND`id`=:abuseId";
+                    $result = $db->execute($stmt, [":assetId" => $_GET["AssetID"], ":abuseId" => $_GET["AbuseID"]]);
+                    if ($result->rowCount() > 0) {
+                        $stmt = "UPDATE reports SET `handled`=1 WHERE `id`=:abuseId";
+                        $db->execute($stmt, [":abuseId" => $_GET["AbuseID"]]);
+                    }
+                }
             }
         }
     }
